22fdf1632053330b6efbcfc6ac26dec87f2594a4
Apara1330 <polapara1330@gmail.com>
2022-02-19 17:51:23 +0530

    [Apara]- Employee is Present or Absent completed

---
+++ EmployeWage/EmployeWage/EmpWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWage
+{
+    public class EmpWage
+    {
+        public static void IffElse()
+        {
+            Random random = new Random();
+            int RandomNumber = random.Next(0,2);
+
+
+            if (RandomNumber == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeWage/EmployeWage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage.IffElse();
+            Console.ReadLine(); 
+
+        }
+    }
+}
+
+
